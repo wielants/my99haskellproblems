1fa03661daf3f746b401775da06008c226b016ba
wielants <wielants@users.noreply.github.com>
2017-12-03 00:21:04 +0100

    added QuickCheck tests (with preconditions, explicit type declaration), improved test description

--- test/Problem1Spec.hs
+++ test/Problem1Spec.hs
@@ -8,7 +8,13 @@ main :: IO ()
 main = do
   hspec $ do
     describe "myLast" $ do
-      it "returns last element" $ do
+      it "returns last element of [1,2,3,4]" $ do
          myLast [1,2,3,4] `shouldBe` 4
-      it "returns last element" $ do
+      it "returns last element of [x,y,z]" $ do
          myLast ['x','y','z'] `shouldBe` 'z'
+      it "returns last element of random [Char]" $
+        property $ \xs -> not (null xs) ==>
+          myLast xs == (last xs :: [Char])
+      it "returns last element of random [Integer]" $
+        property $ \xs -> not (null xs) ==>
+          myLast xs == (last xs :: [Integer])
--- test/Problem2Spec.hs
+++ test/Problem2Spec.hs
@@ -8,7 +8,10 @@ main :: IO ()
 main = do
   hspec $ do
     describe "myButLast" $ do
-      it "returns last but one element" $ do
+      it "returns last but one element of [1,2,3,4]" $ do
          myButLast [1,2,3,4] `shouldBe` 3
-      it "returns last but one element" $ do
+      it "returns last but one element of [x,y,z]" $ do
          myButLast ['x','y','z'] `shouldBe` 'y'
+      it "returns last but one element of random [Char]" $
+        property $ \xs -> length xs > 1 ==>
+          myButLast xs == (last (init xs) :: [Char])
